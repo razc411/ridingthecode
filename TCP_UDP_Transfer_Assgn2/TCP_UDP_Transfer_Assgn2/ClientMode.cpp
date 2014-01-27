17583805b25a672f079bd9c037aa7acc3c55ced6
razc411 <razc411@hotmail.ca>
2014-01-27 00:00:44 -0700

    Sleep Time.

--- TCP_UDP_Transfer_Assgn2/TCP_UDP_Transfer_Assgn2/ClientMode.cpp
+++ TCP_UDP_Transfer_Assgn2/TCP_UDP_Transfer_Assgn2/ClientMode.cpp
@@ -1,2 +1,11 @@
 #include "stdafx.h"
 #include "TCP_UDP_Transfer_Assgn2.h"
+
+void init_client(){
+
+}
+
+void send_data(){
+
+}
+
--- TCP_UDP_Transfer_Assgn2/TCP_UDP_Transfer_Assgn2/ServerMode.cpp
+++ TCP_UDP_Transfer_Assgn2/TCP_UDP_Transfer_Assgn2/ServerMode.cpp
@@ -1,10 +1,15 @@
 #include "stdafx.h"
 #include "TCP_UDP_Transfer_Assgn2.h"
 
-void init_client(){
+void init_server(){
 
 }
 
 void recieve_data(){
 
 }
+
+void save_data(){
+
+}
+
