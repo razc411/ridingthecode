dd6bde8295ec8afb30ae3d214e85f0e1af34ed43
Raz <razc411@hotmail.ca>
2014-03-20 11:11:18 -0700

    Started changing up client to work with server. Can probably join channels now.

--- LinuxAssign3/client/src/SystemRouter.cpp
+++ LinuxAssign3/client/src/SystemRouter.cpp
@@ -18,18 +18,15 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 
 int num_channels = 1;
+int current_channel = 1;
+static pthread_t thread_input[MAX_CHANNELS];
+static int input_pipe[MAX_CHANNELS][2];
 
 int main()
 {
-    int sd, port;
-    struct hostent  *hp;
-    struct sockaddr_in server;
-
     pthread_t 	thread_recv;
     pthread_t 	thread_send;
-    pthread_t   thread_input[MAX_CHANNELS];
 
-    int         input_pipe[MAX_CHANNELS][2];
     int 		recv_pipe[2];
     int         send_pipe[2];
   
@@ -40,18 +37,13 @@ int main()
     uint32_t    type;
 
 	pipe(input_pipe[MAIN_CHANNEL]);
-	pipe(recv_pipe);
-    pipe(send_pipe);
     
     dispatch_thread(InputManager, (void*)input_pipe, &thread_input[i]); // main channel thread
 
-    max_fd = input_pipe[MAIN_CHANNEL][READ] > recv_pipe[READ] ? input_pipe[MAIN_CHANNEL][READ] > recv_pipe[READ];
-    max_fd = max_fd > send_pipe[READ] ? max_fd : send_pipe[READ];
-	
+    max_fd = input_pipe[MAIN_CHANNEL][READ];
+   
     FD_ZERO(&listen_fds);
     FD_SET(input_pipe[MAIN_CHANNEL][READ], &listen_fds);
-    FD_SET(send_pipe[READ], &listen_fds);
-    FD_SET(recv_pipe[READ], &listen_fds);
 
     type = -1;
 
@@ -61,12 +53,6 @@ int main()
         active = listen_fds;
     	ret = select(max_fd + 1, &active, NULL, NULL, NULL);
 
-        //connect to the remote server
-        if(connect_to_server(sd, port, server, *hp) != 0_
-        {
-            printf("Failed to connect, exiting program.\n");
-            exit(1);
-        }
         for(int i = 0; i < num_channels; i++)
         {
             if(ret && FD_ISSET(input_pipe[i][READ], &active))
@@ -75,8 +61,7 @@ int main()
 
                 else if(type == JOIN_CHANNEL)
                 {
-                   
-                   //join_channel();
+                    join_channel(&listen_fds, &max_fd);
                 }
 
                 else if(type == QUIT_CHANNEL)
@@ -86,7 +71,7 @@ int main()
 
                 else if(type == CLIENT_MSG)
                 {
-                    //send c_msg_pkt to send thread
+                    
                     if(tcp_send(sd, sizeof(C_MSG_PKT), (char*)info_packet) != CLIENT_MSG)
                     {
                         perror("Failed to send message");
@@ -131,8 +116,6 @@ int main()
             printf("Failure in send thread, exiting program.\n");
             exit(3);
         }
-
-
     }
 
     for(int i = 0; i < num_channels; i++)
@@ -140,44 +123,11 @@ int main()
         close(input_pipe[i][READ]);
         close(input_pipe[i][WRITE]);
     }
+
     close(recv_pipe[READ]);
     close(recv_pipe[WRITE]);
     close(send_pipe[READ]);
     close(send_pipe[WRITE]);
-
-    return 0;
-}
-
-int connect_to_server(int sd, int port, sockaddr_in server, hostent *hp){
-    // Create the socket
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        perror("Cannot create socket");
-        exit(1);
-    }
-
-    // clear memory for server socket
-    bzero((char *)&server, sizeof(struct sockaddr_in));
-
-    // initialize server socket
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-
-    // get host ip
-    if ((hp = gethostbyip(host)) == NULL)
-    {
-        fprintf(stderr, "Unknown server ip address\n");
-        exit(1);
-    }
-    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-
-    // Connecting to the server
-    if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
-    {
-        fprintf(stderr, "Can't connect to server\n");
-        perror("connect");
-        exit(1);
-    }
 
     return 0;
 }
@@ -198,16 +148,100 @@ int connect_to_server(int sd, int port, sockaddr_in server, hostent *hp){
 --      Intializes the client, the type of intialization depends on the chosen protocol in the settings. Binds whenever
 --      the connection is TCP.
 ----------------------------------------------------------------------------------------------------------------------*/
-void join_channel(int cm_pipe)
-{
-    C_JOIN_PKT * info_packet = (C_JOIN_PKT*)malloc(sizeof(C_JOIN_PKT));
+int connect_to_server(){
+    int sd = -1;
+    struct hostent  *hp;
+    struct sockaddr_in server;
 
-    //send client join pkt to send
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Cannot create socket");
+        exit(1);
+    }
+
+    bzero((char *)&server, sizeof(struct sockaddr_in));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(TCP_PORT);
+
+    if ((hp = gethostbyip("127.0.0.1")) == NULL)
+    {
+        fprintf(stderr, "Unknown server ip address\n");
+        exit(1);
+    }
+    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+
+    // Connecting to the server
+    if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+    {
+        fprintf(stderr, "Can't connect to server\n");
+        perror("connect");
+        exit(1);
+    }
+
+    return sd;
+}
+/*------------------------------------------------------------------------------------------------------------------
+--      FUNCTION: init_client
+--
+--      DATE: Febuary 6 2014
+--      REVISIONS: none
+--
+--      DESIGNER: Ramzi Chennafi
+--      PROGRAMMER: Ramzi Chennafi
+--
+--      INTERFACE: void init_client(HWND hwnd) , takes the parent HWND as an argument.
+--
+--      RETURNS: void
+--
+--      NOTES:
+--      Intializes the client, the type of intialization depends on the chosen protocol in the settings. Binds whenever
+--      the connection is TCP.
+----------------------------------------------------------------------------------------------------------------------*/
+void join_channel(int * listen_fds, int * max_fd)
+{
+    int sd;
+    int input_pipe[2];
+    C_JOIN_PKT * info_packet;
+    S_CHANNEL_INFO_PKT * c_info_packet;
+    
+    if((sd = connect_to_server()) != 0)
+    {
+        printf("Failed to connect, exiting program.\n");
+        return;
+    }
+    
     if(tcp_send(sd, sizeof(C_JOIN_PKT), (char*)info_packet) != CLIENT_JOIN)
     {
         perror("Failed to join channel");
         return;
     }
+
+    if(tcp_recieve(sd, (char*)c_info_packet) < 0)
+    {
+        printf("Failed to retrieve channel info packet.\n");
+    }
+    
+    if(c_info_packet.code == CONNECTION_REJECTED)
+    {
+        printf("Channel join failed: Server connection rejected.");
+    }
+
+    pipe(input_pipe);
+    input_pipes[current_channel][READ] = input_pipe[READ];
+    input_pipes[current_channel++][WRITE] = input_pipe[WRITE];
+
+    CHANNEL_DATA cdata;
+    cdata.write_pipe = input_pipe[WRITE];
+    cdata.read_pipe = input_pipe[READ];
+    memcpy(cdata.channelname, c_info_packet->channel_name, sizeof(c_info_packet->chanel_name));
+    memcpy(cdata.client_list, c_info_packet->channel_clients, sizeof(c_info_packet->channel_clients));
+
+    *max_fd = *max_fd > input_pipes[current_channel][READ] ? *max_fd : input_pipes[current_channel][READ];
+    FD_SET(input_pipes[current_channel][READ], listen_fds);
+
+    dispatch_thread(InputManager, (void*)cdata, &thread_input[i]);
+    num_channels++;
 }
 /*------------------------------------------------------------------------------------------------------------------
 --      FUNCTION: init_client
@@ -275,90 +309,6 @@ void add_client(int cm_pipe)
     
     num_clients++;
     printf("Client passed to channel for adding.\n");
-}
-/*------------------------------------------------------------------------------------------------------------------
---      FUNCTION: init_client
---
---      DATE: Febuary 6 2014
---      REVISIONS: none
---
---      DESIGNER: Ramzi Chennafi
---      PROGRAMMER: Ramzi Chennafi
---
---      INTERFACE: void init_client(HWND hwnd) , takes the parent HWND as an argument.
---
---      RETURNS: void
---
---      NOTES:
---      Intializes the client, the type of intialization depends on the chosen protocol in the settings. Binds whenever
---      the connection is TCP.
-----------------------------------------------------------------------------------------------------------------------*/
-void add_channel(CHANNEL_DATA * chdata, int * max_fd, fd_set * listen_fds, int input_pipe)
-{
-    char temp[MAX_CHANNEL_NAME];
-
-    read_pipe(input_pipe, &temp, MAX_CHANNEL_NAME);
-    memcpy(channel_name_list[open_channels], temp, MAX_CHANNEL_NAME);
-    chdata->channel_num = open_channels;
-
-	pipe(channel_pipes[open_channels]);
-	chdata->write_pipe = channel_pipes[open_channels][WRITE];
-	chdata->read_pipe = channel_pipes[open_channels][READ];
-
-	if(open_channels > last_channel)
-    {
-    	*max_fd = *max_fd > channel_pipes[last_channel][READ] ? *max_fd : channel_pipes[last_channel][READ];
-    	FD_SET(channel_pipes[last_channel][READ], listen_fds);
-	}
-
-	last_channel = open_channels;
-
-	dispatch_thread(ChannelManager, (void*)chdata, &thread_channel[open_channels++]);
-
-    printf("Channel Added. Thread now open.\n");
-}
-/*------------------------------------------------------------------------------------------------------------------
---      FUNCTION: init_client
---
---      DATE: Febuary 6 2014
---      REVISIONS: none
---
---      DESIGNER: Ramzi Chennafi
---      PROGRAMMER: Ramzi Chennafi
---
---      INTERFACE: void init_client(HWND hwnd) , takes the parent HWND as an argument.
---
---      RETURNS: void
---
---      NOTES:
---      Intializes the client, the type of intialization depends on the chosen protocol in the settings. Binds whenever
---      the connection is TCP.
-----------------------------------------------------------------------------------------------------------------------*/
-void kick_client(int input_pipe)
-{
-    char clientname[MAX_USER_NAME];
-    char channel_name[MAX_CHANNEL_NAME];
-    char msg[MAX_STRING];
-    uint32_t type = CLIENT_KICK;
-
-    read_pipe(input_pipe, &clientname, MAX_USER_NAME);
-    read_pipe(input_pipe, &channel_name, MAX_CHANNEL_NAME);
-    read_pipe(input_pipe, &msg, MAX_STRING);
-    
-    for(int i = 0; i < open_channels; i++)
-    {
-        if(strcmp(channel_name_list[i], channel_name) == 0)
-        {
-            write_pipe(channel_pipes[i][WRITE], &type, TYPE_SIZE);    
-            write_pipe(channel_pipes[i][WRITE], clientname, MAX_USER_NAME);
-            write_pipe(channel_pipes[i][WRITE], msg, MAX_STRING);
-            break;
-        }
-    }
-        
-    --num_clients;
-
-    printf("Client kick msg passed to channel.\n");
 }
 /*------------------------------------------------------------------------------------------------------------------
 --      FUNCTION: init_client
@@ -445,33 +395,30 @@ void close_server(int cm_pipe[2], int input_pipe[2])
 ----------------------------------------------------------------------------------------------------------------------*/
 void* InputManager(void * indata)
 {
-    CMANAGERDATA * input_data = (CMANAGERDATA*) indata;
+    THREAD_DATA * input_data = (THREAD_DATA*) indata;
 
     uint32_t type;
     char *   temp = NULL;
     char     cmd[MAX_STRING];
-    char *   channelname;
-    char *   name;
+    char     channelname[MAX_CHANNEL_NAME];
     size_t   nbytes = MAX_STRING;
 
     while(getline(&temp, &nbytes, stdin))
     {
         sscanf(temp, "%s", cmd);
         
-        if(strcmp(cmd, "/create") == 0)
+         if(strcmp(cmd, "/create") == 0)
         {
             type = CHANNEL_CREATE;
+            sscanf(temp, "%s %s", cmd, channelname);
             write_pipe(input_data->write_pipe, &type, TYPE_SIZE);
-            sscanf(temp, "%s %s %s", cmd, channelname);
             write_pipe(input_data->write_pipe, channelname, MAX_CHANNEL_NAME);
         }
         else if(strcmp(cmd, "/kick") == 0)
         {
-            type = CLIENT_KICK;
-            write_pipe(input_data->write_pipe, &type, TYPE_SIZE);
-            sscanf(temp, "%s %s %s", cmd, channelname, name);
-            write_pipe(input_data->write_pipe, channelname, MAX_CHANNEL_NAME);
-            write_pipe(input_data->write_pipe, name, MAX_USER_NAME);
+            S_KICK_PKT s_kick;
+            sscanf(temp, "%s %s %s %s", cmd, s_kick.channel_name, s_kick.client_name, s_kick.msg);
+            write_packet(input_data->write_pipe, CLIENT_KICK, &s_kick);
         }
         else if(strcmp(cmd, "/exit") == 0)
         {
@@ -479,12 +426,11 @@ void* InputManager(void * indata)
             write_pipe(input_data->write_pipe, &type, TYPE_SIZE);
             break;
         }
-        else if(strcmp(cmd, "/close") == 0)
+        else
         {
-            type = CHANNEL_CLOSE;
-            write_pipe(input_data->write_pipe, &type, TYPE_SIZE);
-            sscanf(temp, "%s %s", cmd, channelname);
-            write_pipe(input_data->write_pipe, channelname, MAX_CHANNEL_NAME);
+            //send message
         }
     }
+
+    return NULL;
 }
--- LinuxAssign3/client/src/client_defs.h
+++ LinuxAssign3/client/src/client_defs.h
@@ -1,29 +1,39 @@
 #ifndef CLIENT_DEFS_H
 #define CLIENT_DEFS_H
-//#include "../server/Packets.h"
 
-#define MAIN_CHANNEL 0
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 //#include <string.h>
 //#include <semaphore.h>
-//#include <pthread.h>
+#include <pthread.h>
 //#include <SDL2/SDL_net.h>
 //#include <SDL2/SDL.h>
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_ttf.h"
+//#include "SDL2/SDL.h"
+//#include "SDL2/SDL_image.h"
+//#include "SDL2/SDL_ttf.h"
 #include <string>
-//#include "packets.h"
+#include "../../server/src/packets.h"
+#include "../../server/src/utils.h"
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 768
-#define SCREEN_BPP 32
+#define SCREEN_WIDTH 	1280
+#define SCREEN_HEIGHT 	768
+#define SCREEN_BPP 		32
+#define MAIN_CHANNEL 	0
 
 typedef struct{
-	
-} t_client_data;
+	int write_pipe;
+	int read_pipe;
+} THREAD_DATA;
+
+typedef struct{
+	int write_pipe;
+	int read_pipe;
+	char * channelname;
+	char * client_list;
+} CHANNEL_DATA;
+
+int connect_to_server();
+void join_channel();
 
 #endif
