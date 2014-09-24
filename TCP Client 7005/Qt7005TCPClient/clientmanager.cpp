f1f8afbecc79b5164cd44cbb17353f5e02468417
razc411 <razc411@hotmail.ca>
2014-09-24 16:39:13 -0700

    Added clientmanager files.

---
+++ TCP Client 7005/Qt7005TCPClient/clientmanager.cpp
@@ -0,0 +1,19 @@
+#include "clientmanager.h"
+
+ClientManager::ClientManager(Ui::MainWindow *ui, QObject *parent):
+    ui(ui), QObject(parent)
+{
+    connect(ui->connect, SIGNAL(pressed()), this, SLOT(StartClient()));
+    connect(ui->disconnect, SIGNAL(pressed()), this, SLOT(StopClient()));
+}
+
+void ClientManager::StartClient()
+{
+    thread = new ClientConnector(ui);
+    thread->start();
+}
+
+void ClientManager::StopClient()
+{
+    thread->exit();
+}
